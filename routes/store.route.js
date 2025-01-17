54c7bfb631cbaf195380161735b88052d8e57d46
allAboutJS <victoronah1914@gmail.com>
2025-01-17 09:16:54 +0100

    Refactor store routes to use more descriptive endpoints for create, update, and delete operations

--- routes/store.route.js
+++ routes/store.route.js
@@ -4,19 +4,12 @@ import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// Get all stores
 router.get("/", authMiddleware, storeController.getAllStores);
-
-// Get a single store by ID
 router.get("/:id", authMiddleware, storeController.getStoreById);
-
-// Create a new store
-router.post("/", authMiddleware, storeController.createStore);
-
-// Update a store by ID
-router.put("/:id", authMiddleware, storeController.updateStore);
-
-// Delete a store by ID
+router.post("/create", authMiddleware, storeController.createStore);
+router.put("/update/:id", authMiddleware, storeController.updateStore);
+router.put("/update/:id/name", authMiddleware, storeController.updateStoreName);
+router.put("/update/:id/description", authMiddleware, storeController.updateStoreDescription);
 router.delete("/:id", authMiddleware, storeController.deleteStore);
 
 export default router;
