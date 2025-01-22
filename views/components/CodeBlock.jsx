79927c535a9db02bed253f6a335126c0219ceec2
allAboutJS <victoronah1914@gmail.com>
2025-01-22 08:55:37 +0100

    Add copy functionality to code blocks and integrate Sonner for notifications

--- views/components/CodeBlock.jsx
+++ views/components/CodeBlock.jsx
@@ -1,8 +1,24 @@
 import hljs from "highlight.js";
+import { toast } from "sonner";
 
 export default function CodeBlock({ code, heading, language }) {
+    const copyCode = async () => {
+        try {
+            await window.navigator.clipboard.writeText(code);
+            toast.success("Text copied!");
+        } catch (error) {
+            toast.error("Failed to copy");
+        }
+    };
+
     return (
-        <div className="code-block">
+        <div className="code-block relative">
+            <button
+                className="absolute right-2 top-2 text-xs px-2 py-1 rounded-sm active:scale-95 bg-zinc-700"
+                onClick={copyCode}
+            >
+                Copy
+            </button>
             <h3>{heading}</h3>
             <pre>
                 <code dangerouslySetInnerHTML={{ __html: hljs.highlight(code, { language }).value }}></code>
--- views/main.jsx
+++ views/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Toaster } from "sonner";
 import "./main.css";
 import "highlight.js/styles/night-owl.min.css";
 
@@ -9,5 +10,6 @@ const rootElement = document.getElementById("app");
 ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
         <App />
+        <Toaster richColors position="top-center" />
     </React.StrictMode>
 );
